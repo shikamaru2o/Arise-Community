0a17adf275874dea7679069aefa0db66622532a1
shikamaru2o <d.joelramesh@gmail.com>
2026-08-28 13:05:28 +0530

    Use updated Arise logo asset

--- arise-site/src/AriseCommunitySite.jsx
+++ arise-site/src/AriseCommunitySite.jsx
@@ -11,7 +11,7 @@ const INSTAGRAM_URL = import.meta.env.VITE_INSTAGRAM_URL || "https://www.instagr
 const YOUTUBE_URL = import.meta.env.VITE_YOUTUBE_URL || "https://youtube.com/@arisecommunityindia?si=I60nStg4PXCN30Ut";
 
 const IMG = {
-  logo: "/images/arise-logo.png",
+  logo: "/images/arise-logo-new.png",
   patta: "/images/speakers/patta.webp",
   bonny: "/images/speakers/bonny.webp",
   joseph: "/images/artists/joseph.webp",
